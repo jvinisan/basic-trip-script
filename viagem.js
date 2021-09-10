909498f4c3d90a7192306e2e6c80acc1da73aaac
João V. S. S <joaoviniciussouzasilva@gmail.com>
2021-09-10 15:25:57 -0300

    Update

---
+++ viagem.js
@@ -0,0 +1,11 @@
+console.log('Bem-vindo à Viagem S.A')
+
+const listaDePacotes = new Array(
+  'Brasília',
+  'São Paulo',
+  'Rio de Janeiro',
+  'La Paz',
+  'Tóquio',
+  'Buenos Aires',
+  'Assunção',
+)
